af0c150950d8416ead8d1004d69bd77b7713b10f
Serhii Lekhman <8.lsv.888@gmail.com>
2023-12-12 17:30:42 +0100

    add bot.sendMessage

---
+++ index.js
@@ -0,0 +1,36 @@
+// Name of bot - namhels_test_bot
+// Use this token to access the HTTP API: 6776775082:AAGUpEa7a3fjpuf9JSBUB7x4JRbZcWKcsAQ
+
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = "6776775082:AAGUpEa7a3fjpuf9JSBUB7x4JRbZcWKcsAQ";
+const webAppUrl = "https://www.google.com/";
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text === "/start") {
+    await bot.sendMessage(
+      chatId,
+      "A button will appear below, fill out the form",
+      {
+        reply_markup: {
+          keyboard: [[{ text: "Fill out the form", web_app: {url: webAppUrl} }]],
+        },
+      }
+    );
+    
+		await bot.sendMessage(
+      chatId,
+      "Go to our online store by clicking the button below",
+      {
+        reply_markup: {
+          inline_keyboard: [[{ text: "Make an order", web_app: {url: webAppUrl} }]],
+        },
+      }
+    );
+  }
+});
